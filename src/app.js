04c4bdbe55252bc036d644183587e21180cf69ad
Balance8 <mslocum819@gmail.com>
2019-12-16 11:39:33 -0800

    serving static files practice

--- src/app.js
+++ src/app.js
@@ -1,21 +1,19 @@
+const path = require('path');
 const express = require('express');
 
+console.log(__dirname);
+console.log(path.join(__dirname, '../public'));
+
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.get('', (req, res) => {
-  res.send('hello express');
-});
-
-app.get('/help', (req, res) => {
-  res.send('Help Page');
-});
-
-app.get('/about', (req, res) => {
-  res.send('About Page');
-});
+app.use(express.static(publicDirectoryPath));
 
 app.get('/weather', (req, res) => {
-  res.send('Weather Page');
+  res.send({
+    forcast: 'test',
+    location: 'test'
+  });
 });
 
 // app.com
