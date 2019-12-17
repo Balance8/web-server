1bdcf2606fceb0ba0c0e0b17cbb3a4b9507dcced
Balance8 <mslocum819@gmail.com>
2019-12-17 12:35:43 -0800

    default params

--- src/app.js
+++ src/app.js
@@ -48,21 +48,24 @@ app.get('/weather', (req, res) => {
     });
   }
 
-  geocode(req.query.address, (error, { latitude, longitude, location }) => {
-    if (error) {
-      return res.send({ error });
-    }
-    forecast(latitude, longitude, (error, { currently }) => {
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
       if (error) {
         return res.send({ error });
       }
-      res.send({
-        forecast: currently,
-        location,
-        address: req.query.address
+      forecast(latitude, longitude, (error, { currently }) => {
+        if (error) {
+          return res.send({ error });
+        }
+        res.send({
+          forecast: currently,
+          location,
+          address: req.query.address
+        });
       });
-    });
-  });
+    }
+  );
 });
 
 app.get('/products', (req, res) => {
