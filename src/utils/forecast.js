255bb77c8d1944a03681b5fc5e583c544aeed531
Balance8 <mslocum819@gmail.com>
2019-12-17 18:30:04 -0800

    wiring up user interface

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,23 +1,17 @@
-const request = require('request');
+const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-  const url = `https://api.darksky.net/forecast/3b22ee6d97112360bbfc136bdb59240e/${encodeURIComponent(
-    latitude
-  )},${encodeURIComponent(longitude)}`;
+    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
 
-  request({ url, json: true }, (error, { body }) => {
-    if (error) {
-      callback('Unable to connect to weather service!');
-    } else if (body.error) {
-      callback('Unable to find location. Try another search');
-    } else {
-      const data = body;
-      callback(undefined, {
-        summary: data.daily.data[0].summary,
-        currently: `Currently it is ${data.currently.temperature} degrees out there.  There is a ${data.currently.precipProbability}% chance of rain`
-      });
-    }
-  });
-};
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
 
-module.exports = forecast;
+module.exports = forecast
